e967361d8b1c3e80962b48d94c605ebf77dd02ce
Marcos Vinícius de Souza Padilha <mpadilhat@gmail.com>
2021-07-04 20:02:58 -0300

    Projeto LetMeAsk responsivo

---
+++ src/components/RoomCode.tsx
@@ -0,0 +1,26 @@
+import copyImg from "../assets/images/copy.svg";
+import "../styles/room-code.scss";
+
+type RoomCodeProps = {
+  code: string;
+};
+
+export function RoomCode(props: RoomCodeProps) {
+  function copyRoomCodeToClipBoard() {
+    navigator.clipboard.writeText(props.code);
+  }
+
+  return (
+    <button className="room-code" onClick={copyRoomCodeToClipBoard}>
+      <div>
+        <img
+          src={copyImg}
+          alt="Copiar código da sala"
+          title="Copiar código da sala"
+        />
+      </div>
+
+      <span>Sala #{props.code}</span>
+    </button>
+  );
+}
